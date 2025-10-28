851a1e1acd14370892eb08fec8a01aec14e6d0be
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-10-28 16:56:01 +0300

    1

--- ex1.c
+++ ex1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
+int main() {
+    printf("Guess the number I'm thinking of which is between 1 and 5 \n");
+    int number; 
+    scanf("%d", &number);
+
+    if (number == 3) {
+        printf("You guessed it right! \n");
+    }
+    else {
+        printf("Wrong guess. Try again! \n");
+    }
+
+    return 0;
+}
--- ex4.c
+++ ex4.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
+int main() {
+    for (int sayi = 2; sayi <= 10; sayi = sayi + 2) {
+        printf("%d\n", sayi);
+    }
+    return 0;
+}
